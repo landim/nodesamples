7e046528e0fa71a830856f2665ddf17224aed15c
landim <landim.arthur@gmail.com>
2015-08-31 01:21:49 -0300

    adding split message handle

--- net-watcher.js
+++ net-watcher.js
@@ -7,10 +7,15 @@ const
 	server = net.createServer(function(connection){
 		console.log('Subscriber connected!');
 
-		connection.write("Now watching " + filename + " for changes...\n");
+		connection.write(JSON.stringify({
+				type:'watching', 
+				file: filename})+ "\n");
 
 		let watcher = fs.watch(filename, function() {
-			connection.write("File " +  filename + " has changed: " + Date.now() +  "\n");
+			connection.write(JSON.stringify({
+				type:'changed', 
+				file: filename,
+				timestamp: Date.now()}) +  "\n");
 		});
 
 		connection.on('close', function() {
